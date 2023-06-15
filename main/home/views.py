e60bc1b09da2a71063d2255e50904154adec5341
Afsal <afsalat9@gmail.com>
2023-06-15 23:03:17 +0530

    sign-in validation proccess all setted but login status message not solved

--- main/home/views.py
+++ main/home/views.py
@@ -77,38 +77,69 @@ def log_leatest_detail(request, id, uname):
 
 
 def registeration(request):
-
-    if request.method == "POST":
-        username = request.POST['username']
-        email_id = request.POST['email']
-        password = request.POST['password']
-        # re_password = request.POST['repassword']
-        status = "logged"
-        customer = register.objects.create(username=username,email_id=email_id,password=password,status=status)
-        customer.save()
-
-        idd = register.objects.filter(username=username)
-
-        for i in idd:
-            id=i.id
-            name=i.username
+    
+    cat_li = category.objects.all()
         
-        cat_list = category.objects.all()
+    featured_p = featured_product.objects.all()
         
-        featured_pro = featured_product.objects.all()
+    leatest_p = leatest_product.objects.all()
         
-        leatest_pro = leatest_product.objects.all()
-        
-        footer_detail = footer_details.objects.all()
+    footer_deta = footer_details.objects.all()
 
-        return render(request,'index.html',{
-            'cate':cat_list,
-            'f_pro':featured_pro,
-            'l_pro':leatest_pro,
-            'lxl':footer_detail,
-            'id':id,
-            'uname':name,
-            })
-
+    # if request.method == "POST":
+    
+    try:
+        if request.method=='POST':
+            if request.POST['email']:
+                username = request.POST['username']
+                email_id = request.POST['email']
+                password = request.POST['password']
+                # re_password = request.POST['repassword']
+                status = "logged"
+                customer = register.objects.create(username=username,email_id=email_id,password=password,status=status)
+                customer.save()
+                
+                idd = register.objects.filter(username=username)
+                
+                for i in idd:
+                    id = i.id
+                    name=i.username
+                    
+                return render(request,'index.html',{
+                    'cate':cat_li,
+                    'f_pro':featured_p,
+                    'l_pro':leatest_p,
+                    'lxl':footer_deta,
+                    'id':id,
+                    'uname':name
+                    })
+    except:
+            if request.method=='POST':
+                if request.POST['lemail']:
+                    email_i_d = request.POST['lemail']
+                    password = request.POST['lpassword']
+                    
+                    if register.objects.filter(email_id=email_i_d) and register.objects.filter(password=password):
+                        # status = 'logged'
+                        # mer = register.objects.all()
+                        # for cust in mer:
+                        #     customers = cust.email_i_d
+                        # customers.update(status=status)    
+                        # customers.save()
+                        
+                        n = register.objects.filter(email_id=email_id)
+                        
+                        for i in n:
+                            name = i.username
+                    
+                    return render(request,'index.html',{
+                        'cate':cat_li,
+                        'f_pro':featured_p,
+                        'l_pro':leatest_p,'lxl':footer_deta,
+                        'uname':name
+                    })
+    else:
+        return render(request, 'sign-up_in.html')            
+            
         
     return render(request, 'sign-up_in.html')

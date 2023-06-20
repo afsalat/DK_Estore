a59e65ad848e060b6b49a2ade42f26eb112b9c9a
Afsal <afsalat9@gmail.com>
2023-06-20 22:43:39 +0530

    sign-up and sign-in validation process fully completed

--- main/home/views.py
+++ main/home/views.py
@@ -96,30 +96,55 @@ def registeration(request):
                 email_id = request.POST['email']
                 password = request.POST['password']
                 re_password = request.POST['repassword']
-
+                
                 if password == re_password:
                     
-                    status = "logged"
-                    customer = register.objects.create(username=username,email_id=email_id,password=password,status=status)
-                    customer.save()
-                    
-                    idd = register.objects.filter(username=username)
-                
-                    for i in idd:
-                        id = i.id
-                        name=i.username
+                    if register.objects.filter(username=username):
+
+                        wr = {
+                            'class':'er',
+                            'message':' Username already exist try another one !'
+                            }
                         
-                    return render(request,'index.html',{
-                        'cate':cat_li,
-                        'f_pro':featured_p,
-                        'l_pro':leatest_p,
-                        'lxl':footer_deta,
-                        'id':id,
-                        'uname':name
-                        })
+                        return render(request, 'sign-up_in.html', {'wr':wr})
+                    
+
+                    elif register.objects.filter(email_id=email_id):
+                            wr = {
+                                'class':'er',
+                                'message':' Email ID already exist try another one !'
+                                }
+                            
+                            return render(request, 'sign-up_in.html', {'wr':wr})
+
+
+                    else:
+                            
+                            status = "logged"
+                            customer = register.objects.create(username=username,email_id=email_id,password=password,status=status)
+                            customer.save()
+                            
+                            idd = register.objects.filter(username=username)
+                
+                            for i in idd:
+                                id = i.id
+                                name=i.username
+                                
+                            return render(request,'index.html',{
+                                'cate':cat_li,
+                                'f_pro':featured_p,
+                                'l_pro':leatest_p,
+                                'lxl':footer_deta,
+                                'id':id,
+                                'uname':name
+                                })
                 else:
-                    wr = """
-                    """
+
+                    wr = {
+                        'class':'er',
+                        'message':' Password is not matching !'
+                    }
+
                     return render(request, 'sign-up_in.html', {'wr':wr})
 
                                 
@@ -146,19 +171,133 @@ def registeration(request):
                         for i in n:
                             name = i.username
                     
-                    return render(request,'index.html',{
-                        'cate':cat_li,
-                        'f_pro':featured_p,
-                        'l_pro':leatest_p,'lxl':footer_deta,
-                        'uname':name
-                    })
-    else:
-        return render(request, 'sign-up_in.html')            
-            
+                        return render(request,'index.html',{
+                            'cate':cat_li,
+                            'f_pro':featured_p,
+                            'l_pro':leatest_p,'lxl':footer_deta,
+                            'uname':name
+                        })
+                
+                    else:
+                        wr = {
+                            'class':'er',
+                            'message':' Invalid Email or Password please try again !'
+                        }
+                        
+                        return render(request, 'sign-up_in.html', {'wr':wr})            
         
+
+
     return render(request, 'sign-up_in.html')
 
 
 def re_registration(request, wr):
+
+    cat_li = category.objects.all()
+        
+    featured_p = featured_product.objects.all()
+        
+    leatest_p = leatest_product.objects.all()
+        
+    footer_deta = footer_details.objects.all()
+
+    
+    try:
+        if request.method=='POST':
+            if request.POST['email']:
+                username = request.POST['username']
+                email_id = request.POST['email']
+                password = request.POST['password']
+                re_password = request.POST['repassword']
+
+                if password == re_password:
+                    
+                    if register.objects.filter(username=username):
+
+                        wr = {
+                            'class':'er',
+                            'message':' Username already exist try another one !'
+                            }
+                        
+                        return render(request, 'sign-up_in.html', {'wr':wr})
+                    
+
+                    elif register.objects.filter(email_id=email_id):
+                            wr = {
+                                'class':'er',
+                                'message':' Email ID already exist try another one !'
+                                }
+                            
+                            return render(request, 'sign-up_in.html', {'wr':wr})
+
+
+                    else:
+                            
+                            status = "logged"
+                            customer = register.objects.create(username=username,email_id=email_id,password=password,status=status)
+                            customer.save()
+                            
+                            idd = register.objects.filter(username=username)
+                
+                            for i in idd:
+                                id = i.id
+                                name=i.username
+                                
+                            return render(request,'index.html',{
+                                'cate':cat_li,
+                                'f_pro':featured_p,
+                                'l_pro':leatest_p,
+                                'lxl':footer_deta,
+                                'id':id,
+                                'uname':name
+                                })
+                else:
+
+                    wr = {
+                        'class':'er',
+                        'message':' Password is not matching !'
+                    }
+
+                    return render(request, 'sign-up_in.html', {'wr':wr})
+
+
+                                
+
+                
+    except:
+            if request.method=='POST':
+                if request.POST['lemail']:
+                    email_i_d = request.POST['lemail']
+                    password = request.POST['lpassword']
+                    
+                    if register.objects.filter(email_id=email_i_d) and register.objects.filter(password=password):
+                        
+                        mr = register.objects.filter(email_id=email_i_d)
+                        for i in mr:
+                            idd = i.id
+
+                        mer = register.objects.get(id=idd)
+                        mer.status = "logged"
+                        mer.save()
+                        
+                        n = register.objects.filter(email_id=email_i_d)
+                        
+                        for i in n:
+                            name = i.username
+                    
+                        return render(request,'index.html',{
+                            'cate':cat_li,
+                            'f_pro':featured_p,
+                            'l_pro':leatest_p,'lxl':footer_deta,
+                            'uname':name
+                        })
+                
+                    else:
+                        wr = {
+                            'class':'er',
+                            'message':' Invalid Email or Password please try again !'
+                        }
+                        
+                        return render(request, 'sign-up_in.html', {'wr':wr})
 
     return render(request, 'sign-up_in.html', {'wr':wr})
